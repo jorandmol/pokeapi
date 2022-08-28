89a22ccef357804ea316ea89a9efbc83a4f5b57a
jorandmol <jandrea@us.es>
2022-08-28 20:29:13 +0200

    Improve database configuration

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-let dbName = process.env.NODE_ENV === 'test' ? 'test' : 'db';
 
 mongoose.connect(
     `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster0.yzjmukd.mongodb.net/?retryWrites=true&w=majority`,
-    { dbName: dbName }
+    { dbName: process.env.NODE_ENV }
 );
